a32762aafab7a0897e216276117e262d510ab85d
arianna choza <unachoza@gmail.com>
2020-05-08 14:09:15 -0400

    react working setting up components

---
+++ src/Pages/HomePage/home.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './home.styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div class="page">
+        <h3>Instructions</h3>
+
+        <p>
+          <strong>
+            In effort to encourage more people to listen to women, I present to you Susan Says, a memory game.
+          </strong>
+          Press the Black Go Button and listen as Susan flashes a sequence of colors. Repeat the pattern by clicking the
+          color buttons.<strong> Remember what Susan told you! </strong>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
