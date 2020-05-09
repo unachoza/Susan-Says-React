f1fe81ee0cb2851edd12a3d1ccd2c34ca6774b4b
arianna choza <unachoza@gmail.com>
2020-05-09 17:51:50 -0400

    these buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import HomePage from 'Pages/HomePage/home.component';
 import GamePage from 'Pages/GamePage/game.component';
+import ScorePage from 'Pages/ScorePage/ScorePage.component';
+import Button from 'Components/Buttons/Button.component.js';
+import 'Components/Buttons/button.styles.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <HomePage /> */}
+      <div className="game-container">
+        <HomePage />
         <GamePage />
+        <ScorePage />
+        <div className="theseButtons">
+          <Button className="button" id="playbutton">
+            Play Game
+          </Button>
+          <Button className="button" id="nextLevel">
+            Next Level
+          </Button>
+          <Button className="button" id="restart">
+            Play Again
+          </Button>
+        </div>
       </div>
     );
   }
---
+++ src/Pages/ScorePage/ScorePage.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import 'Pages/ScorePage/ScorePage.styles.css';
+
+class ScorePage extends Component {
+  render() {
+    return (
+      <div className="page" id="page3">
+        <h5 id="display-score">
+          00 points<br></br>
+          Your Score
+        </h5>
+        <h4>
+          <a href="https://www.linkedin.com/in/arianna-choza-37a44762/" target="blank">
+            created by arianna choza
+          </a>
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default ScorePage;
