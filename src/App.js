020272129ed9c16b4983e78e1bc21338b30d54bc
arianna choza <unachoza@gmail.com>
2020-05-10 10:19:17 -0400

    added pseudocode to game page to rework vanilla js functions to react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import HomePage from 'Pages/HomePage/home.component';
 import GamePage from 'Pages/GamePage/game.component';
 import ScorePage from 'Pages/ScorePage/ScorePage.component';
@@ -7,62 +7,64 @@ import 'Components/Buttons/button.styles.css';
 import Footer from 'Components/Footer/Footer.component';
 import { womensNames } from 'Constants/constants';
 
-class App extends Component {
-  state = {
-    curretPage: 'home',
-  };
+const App = () => {
+  const [curretPage, setCurrentPage] = useState('home')
+ 
 
-  startGame = () => {
-    restart.style.visibility = 'hidden';
-    instructionsPage.style.visibility = 'hidden';
-    gamePage.style.visibility = 'visible';
-    playButton.style.visibility = 'hidden';
-    nextLevel.style.visibility = 'hidden';
-    header.style.visibility = 'hidden';
-    page3.style.visibility = 'hidden';
-    score = 0;
-    colorNum = [];
-    user = [];
+  const startGame = () => {
+    console.log('start gmae was clicked ')
+    
+    setCurrentPage('gamePage')
+    // restart.style.visibility = 'hidden';
+    // instructionsPage.style.visibility = 'hidden';
+    // gamePage.style.visibility = 'visible';
+    // playButton.style.visibility = 'hidden';
+    // nextLevel.style.visibility = 'hidden';
+    // header.style.visibility = 'hidden';
+    // page3.style.visibility = 'hidden';
+    // score = 0;
+    // colorNum = [];
+    // user = [];
     addRandomColorToSequence();
   };
 
-  addRandomColorToSequence = () => {
-    element = Math.floor(Math.random() * 4);
-    colorNum.push(element);
+  const addRandomColorToSequence = () => {
+console.log('addrandome color was clicked')  
+  // element = Math.floor(Math.random() * 4);
+    // colorNum.push(element);
   };
-  nextLevelBackgroundChange = () => {
-    currentBackgroundImage += 1;
-    document.body.style.background = `url(${bImages[currentBackgroundImage]})`;
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundRepeat = 'no-reapeat';
+  const nextLevelBackgroundChange = () => {
+console.log('next level was clicked')  
+  // currentBackgroundImage += 1;
+    // document.body.style.background = `url(${bImages[currentBackgroundImage]})`;
+    // document.body.style.backgroundSize = 'cover';
+    // document.body.style.backgroundRepeat = 'no-reapeat';
   };
-  nextLevelAchieved = () => {
-    gamePage.style.visibility = 'visible';
-    nextLevel.style.visibility = 'hidden';
-    user = [];
+  const nextLevelAchieved = () => {
+console.log('next level acheived was clicked')  
+  // gamePage.style.visibility = 'visible';
+    // nextLevel.style.visibility = 'hidden';
+    // user = [];
   };
-  render() {
-    const { curretPage } = this.state;
     return (
       <div className="game-container">
         {curretPage === 'home' && <HomePage />}
-        {curretPage === 'gamePage' && <GamePage />}
+        {curretPage === 'gamePage' && <GamePage curretPage={curretPage} />}
         {curretPage === 'scorePage' && <ScorePage />}
         <div className="theseButtons">
-          <Button className="button" id="playbutton">
+          <Button className="button" id="playbutton" onClick={() => startGame()}>
             Play Game
           </Button>
           <Button className="button" id="nextLevel">
             Next Level
           </Button>
-          <Button className="button" id="restart">
+          <Button className="button" id="restart" onClick={() => startGame()}>
             Play Again
           </Button>
         </div>
         {/* <Footer womensNames={womensNames} /> */}
       </div>
     );
-  }
 }
 
 export default App;
--- src/Pages/GamePage/game.component.js
+++ src/Pages/GamePage/game.component.js
@@ -4,52 +4,80 @@ import 'Components/Buttons/button.styles.css';
 
 const buttons = [
   {
-    dom: blue,
+    dom: 'blue',
     value: 1,
   },
   {
-    dom: green,
+    dom: 'green',
     value: 2,
   },
   {
-    dom: purple,
+    dom: 'purple',
     value: 3,
   },
   {
-    dom: red,
+    dom: 'red',
     value: 0,
   },
   {
-    dom: go,
+    dom: 'go',
     value: 'go',
   },
 ];
 
-
 class GamePage extends Component {
-  state = {
-    susansColors: [],
-    usersColors: [],
+  constructor() {
+    super();
+    this.state = {
+      susansColors: [],
+      usersColors: [],
+    };
+  }
+  handleGo = () => {
+    console.log('go, we are going!');
+    {/*pseudocode:
+    susansColors needs to increment
+    susansColors needs to call color buttons to blink
+    wait for user response to equal length of susansColors
+    then check win logic
+    if win allow next level to appear
+    if lose show score page  */}
+    
+    this.addRandomColorToSequence()
+    this.clickColorButtons();
+    this.susanBlink();
+  };
+  addRandomColorToSequence = () => {
+    element = Math.floor(Math.random() * 4);
+    susansColors.push(element);
   };
 
   clickColorButtons = () => {
-    for (let i = 0; i < buttons.length - 1; i++) {
-      buttons[i]['dom'].addEventListener('click', () => {
-        user.push(buttons[i]['value']);
-        winOrLoseAfter(colorNum, user);
-      });
+     {/*pseudocode:
+    onclick color needs to be added to userColors array
+    and trigger active animation
+    keep track of sequence lengths to trigger win logic when lengths are equal
+     */}
+        this.state.usersColors.push(buttons[i]['value']);
+        this.winOrLoseAfter(this.state.susansColors, this.state.usersColors);
     }
-  };
-  susanBlink = (colorNum) => {
-    for (let i = 0; i < colorNum.length; i++) {
+
+susanBlink = (susansColors) => {
+       {/*pseudocode:
+   takes susansColors sequence and translates to which colors need to blink, loops through entire arr
+     */}
+    for (let i = 0; i < this.state.susansColors.length; i++) {
       setTimeout(() => {
-        whichColorBlinks(colorNum[i]);
+        this.whichColorBlinks(this.state.susansColors[i]);
       }, i * 500);
     }
   };
- 
-  whichColorBlinks = (colorNum) => {
-    switch (colorNum) {
+
+whichColorBlinks = (susansColors) => {
+       {/*pseudocode:
+  translates sequences arr values to which colors blink 
+     */}
+    switch (this.state.susansColors) {
       case 0:
         setTimeout(function () {
           document.getElementById('red').classList.remove('activeR');
@@ -71,48 +99,54 @@ class GamePage extends Component {
         }, 400);
         break;
     }
-  };
-    winOrLoseAfter = (colorNum, user) => {
+};
+
+winOrLoseAfter = (susansColors, usersColors) => {
+       {/*pseudocode:
+   compares arrays, susan v user,
+   if equal, show next level button
+   else calculate score 
+     */}
     let loser = false;
-    if (user.length === colorNum.length) {
-      for (let i = 0; i < colorNum.length; i++) {
-        if (colorNum[i] !== user[i]) {
+    if (this.state.usersColors.length === this.state.susansColors.length) {
+      for (let i = 0; i < this.state.susansColors.length; i++) {
+        if (this.state.susansColors[i] !== this.state.usersColors[i]) {
           loser = true;
-          gamePage.style.visibility = 'hidden';
-          page3.style.visibility = 'visible';
-          theseButtons.style.margintop = '28vh';
-          userScore.innerHTML = 'Susan Says ' + 'Your Score is ' + score;
-          scorePage.style.visibility = 'visible';
-          restart.style.visibility = 'visible';
+          // gamePage.style.visibility = 'hidden';
+          // page3.style.visibility = 'visible';
+          // theseButtons.style.margintop = '28vh';
+          // this.state.usersColorsScore.innerHTML = 'Susan Says ' + 'Your Score is ' + score;
+          // scorePage.style.visibility = 'visible';
+          // restart.style.visibility = 'visible';
           break;
         }
       }
       if (!loser) {
-        score += colorNum.length * 10;
-        gamePage.style.visibility = 'hidden';
-        nextLevel.style.visibility = 'visible';
+        // score += this.state.susansColors.length * 10;
+        // gamePage.style.visibility = 'hidden';
+        // nextLevel.style.visibility = 'visible';
       }
     }
-  }
+  };
 
   render() {
     return (
       <div className="page" id="page2">
         <div className="susan-container">
-          <Button className="susan-container color top" id="blue">
+          <Button className="color top" id="blue">
             Blue
           </Button>
-          <Button className="susan-container color top" id="green">
+          <Button className="color top" id="green">
             Green
           </Button>
           <br></br>
-          <Button className="susan-container color " id="go">
+          <Button className="color " id="go" onClick={() => this.handleGo()}>
             Go
           </Button>
-          <Button className="susan-container color side" id="purple">
+          <Button className="color side" id="purple">
             Purple
           </Button>
-          <Button className="susan-container color side" id="red">
+          <Button className="color side" id="red">
             Red
           </Button>
         </div>
