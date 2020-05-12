f1b320f5b0c4364ec1a5781bf0ede65b921afc39
arianna choza <unachoza@gmail.com>
2020-05-12 18:24:05 -0400

    changed gamepage to functional component and using hooks, useEffect to fix the aysnc state delay

--- src/Pages/GamePage/game.component.js
+++ src/Pages/GamePage/game.component.js
@@ -4,13 +4,17 @@ import Footer from 'Components/Footer/Footer.component';
 import { ladies } from 'Constants/constants';
 import 'Components/Buttons/button.styles.css';
 
-const GamePage = ({gamePage, showScore}) =>  {
-    const [susansColors, setSusansColors] = useState([])
-    const [usersColors, setUserColors] = useState([])
-    const [activeBlue, setActiveBlue ] = useState(false)  
-    const [activeGreen, setActiveGreen] = useState(false) 
-    const [activeRed, setActiveRed] = useState(false) 
-    const [activePurple, setActivePurple] = useState(false)  
+const GamePage = ({ gamePage, showScore }) => {
+  const [susansColors, setSusansColors] = useState([]);
+  const [usersColors, setUserColors] = useState([]);
+  const [activeBlue, setActiveBlue] = useState(false);
+  const [activeGreen, setActiveGreen] = useState(false);
+  const [activeRed, setActiveRed] = useState(false);
+  const [activePurple, setActivePurple] = useState(false);
+
+  useEffect(() => {
+    susanBlink(susansColors);
+  }, [susansColors]);
   const handleGo = () => {
     console.log('go, we are going!');
     {
@@ -27,64 +31,66 @@ const GamePage = ({gamePage, showScore}) =>  {
   const addRandomColorToSequence = () => {
     let element = Math.floor(Math.random() * 4);
     console.log(element);
-    setSusansColors([...susansColors, element] );
+    setSusansColors([...susansColors, element]);
   };
 
   const clickColorButtons = (e) => {
     let value = e.target.value;
     setUserColors([...usersColors, parseInt(value)]);
-    if (usersColors.length === susansColors.length) this.winOrLoseAfter(susansColors, usersColors);
+    if (usersColors.length === susansColors.length) winOrLoseAfter(susansColors, usersColors);
   };
 
   // pseudocode:
-  //    takes susansColors sequence and translates to which colors need to blink 
-  
-  useEffect((susansColors) => {
-    console.log('because');
-    susansColors.forEach((color, i) =>
-      setTimeout(() => {
-        console.log(color, 'because go was clicked susan should be blinking');
-        whichColorBlinks(color);
-      }, i * 500)
-    );
-  }, 
-  [susansColors]
-  )
+  //    takes susansColors sequence and translates to which colors need to blink
+  const susanBlink = (susansColors) => {
+      console.log(susansColors);
+      susansColors.forEach((color, i) =>
+        setTimeout(() => {
+          console.log(color, 'because go was clicked susan should be blinking');
+          whichColorBlinks(color);
+        }, i * 500)
+      );
+    },
+    //   useEffect(() => {
+    //   susanBlink(susansColors)
+    //   },
 
+    //   [susansColors]
+    // );
 
-  // pseudocode:
-  // translates sequences arr values to which colors blink
-  const whichColorBlinks = (color) => {
-    console.log('checking for blink');
-    switch (color) {
-      case 0:
-        setActiveRed(true)
-        setTimeout(() => {
-          setActiveRed(false)
-        }, 400);
-        break;
-      case 1:
-        setActiveBlue(true)
-        setTimeout(() => {
-          setActiveBlue(false)
-        }, 400);
-        break;
-      case 2:
-        setActiveGreen(true)
-        setTimeout(() => {
-          setActiveGreen(false)
-        }, 400);
-        break;
-      case 3:
-        setActivePurple(true)
-        setTimeout(() => {
-          setActivePurple(false)
-        }, 400);
-        break;
-      default:
-        return;
-    }
-  };
+    // pseudocode:
+    // translates sequences arr values to which colors blink
+    whichColorBlinks = (color) => {
+      console.log('checking for blink');
+      switch (color) {
+        case 0:
+          setActiveRed(true);
+          setTimeout(() => {
+            setActiveRed(false);
+          }, 400);
+          break;
+        case 1:
+          setActiveBlue(true);
+          setTimeout(() => {
+            setActiveBlue(false);
+          }, 400);
+          break;
+        case 2:
+          setActiveGreen(true);
+          setTimeout(() => {
+            setActiveGreen(false);
+          }, 400);
+          break;
+        case 3:
+          setActivePurple(true);
+          setTimeout(() => {
+            setActivePurple(false);
+          }, 400);
+          break;
+        default:
+          return;
+      }
+    };
 
   const winOrLoseAfter = (susan, user) => {
     {
@@ -112,9 +118,9 @@ const GamePage = ({gamePage, showScore}) =>  {
       }
       if (!loser) {
         console.log('you won!!!!');
-        this.nextLevelAchieved();
-        this.nextLevelBackgroundChange();
-        // score += this.state.susansColors.length * 10;
+        nextLevelAchieved();
+        nextLevelBackgroundChange();
+        // score +=state.susansColors.length * 10;
         // gamePage.style.visibility = 'hidden';
         // nextLevel.style.visibility = 'visible';
       }
@@ -133,56 +139,55 @@ const GamePage = ({gamePage, showScore}) =>  {
     // nextLevel.style.visibility = 'hidden';
     // user = [];
   };
-    console.log(this.state);
-    if (usersColors.length === susansColors.length) this.winOrLoseAfter(susansColors, usersColors);
+  if (usersColors.length === susansColors.length) winOrLoseAfter(susansColors, usersColors);
 
-    return (
-      <div className="page" id="page2">
-        <div className="susan-container">
-          <Button
-            className="color top"
-            id="blue"
-            style={activeBlue ? { background: 'rgb(33, 187, 238)' } : null}
-            value={1}
-            onClick={(e) => this.clickColorButtons(e)}
-          >
-            Blue
-          </Button>
-          <Button
-            className="color top"
-            id="green"
-            style={activeGreen ? { background: 'rgb(132, 216, 7)' } : null}
-            value={2}
-            onClick={(e) => this.clickColorButtons(e)}
-          >
-            Green
-          </Button>
-          <br></br>
-          <Button className="color " id="go" value="go" onClick={() => this.handleGo()}>
-            Go
-          </Button>
-          <Button
-            className="color side"
-            id="purple"
-            value={3}
-            style={activePurple ? { background: 'rgb(236, 24, 236)' } : null}
-            onClick={(e) => this.clickColorButtons(e)}
-          >
-            Purple
-          </Button>
-          <Button
-            className="color side"
-            id="red"
-            style={activeRed ? { background: 'rgb(247, 50, 188)' } : null}
-            value={0}
-            onClick={(e) => this.clickColorButtons(e)}
-          >
-            Red
-          </Button>
-        </div>
-        <Footer ladies={ladies} />
+  return (
+    <div className="page" id="page2">
+      <div className="susan-container">
+        <Button
+          className="color top"
+          id="blue"
+          style={activeBlue ? { background: 'rgb(33, 187, 238)' } : null}
+          value={1}
+          onClick={(e) => clickColorButtons(e)}
+        >
+          Blue
+        </Button>
+        <Button
+          className="color top"
+          id="green"
+          style={activeGreen ? { background: 'rgb(132, 216, 7)' } : null}
+          value={2}
+          onClick={(e) => clickColorButtons(e)}
+        >
+          Green
+        </Button>
+        <br></br>
+        <Button className="color " id="go" value="go" onClick={() => handleGo()}>
+          Go
+        </Button>
+        <Button
+          className="color side"
+          id="purple"
+          value={3}
+          style={activePurple ? { background: 'rgb(236, 24, 236)' } : null}
+          onClick={(e) => clickColorButtons(e)}
+        >
+          Purple
+        </Button>
+        <Button
+          className="color side"
+          id="red"
+          style={activeRed ? { background: 'rgb(247, 50, 188)' } : null}
+          value={0}
+          onClick={(e) => clickColorButtons(e)}
+        >
+          Red
+        </Button>
       </div>
-    )
-}
+      <Footer ladies={ladies} />
+    </div>
+  );
+};
 
 export default GamePage;
