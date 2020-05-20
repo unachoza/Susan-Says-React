51c55dd65028437f7535f391e9f7817e50988625
arianna choza <unachoza@gmail.com>
2020-05-19 22:51:25 -0400

    footer

--- src/Components/Footer/Footer.component.js
+++ src/Components/Footer/Footer.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'Components/Footer/Footer.styles.css'
 
-const Footer = ({ ladies }) => <div className="ladys-name">lady{ladies.name}</div>;
+const Footer = ({ ladies }) => <div className="ladys-name">lady{ladies[1].name}</div>;
 
 export default Footer
