c749b41e3a38553ded1bbd019d262aa5d58abd04
arianna choza <unachoza@gmail.com>
2020-07-09 23:45:23 -0400

    instructions

--- src/Constants/Instructions.constant.js
+++ src/Constants/Instructions.constant.js
@@ -1,7 +1,7 @@
 export const instructions = [
   'In effort to encourage more people to listen to women, I present to you Susan Says, a memory game.',
-  'Press the black GO button and repeat the pattern shown',
-  'Remember what Susan Said!',
+  // 'Press the black GO button and repeat the pattern shown',
+  // 'Remember what Susan Said!',
 ];
 
 //   1: 'In effort to encourage more people to listen to women, I present to you Susan Says, a memory game.',
