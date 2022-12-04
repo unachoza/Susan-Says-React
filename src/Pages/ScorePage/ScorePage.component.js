344c46a119ebdb49d797a46d4ff6ae2a9fd0ff37
arianna choza <arianna.choza@splice.com>
2022-12-03 19:35:12 -0500

    added trivia component

--- src/Pages/ScorePage/ScorePage.component.js
+++ src/Pages/ScorePage/ScorePage.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Trivia from 'Components/Trivia/Trivia.component';
 import 'Components/Buttons/button.styles.css';
 import 'Pages/ScorePage/ScorePage.styles.css';
 
@@ -8,6 +9,7 @@ const ScorePage = ({ ladiesIndex }) => {
       <a href="https://www.linkedin.com/in/arianna-choza-37a44762/" target="blank">
         created by arianna choza
       </a>
+      <Trivia ladiesIndex={ladiesIndex}/>
     </div>
   );
 };
