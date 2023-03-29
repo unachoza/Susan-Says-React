8cf452b699a56c27086a70b07be9536b593c5bb5
arianna <unachoza@gmail.com>
2023-03-29 11:54:02 -0400

    formatting for devices

--- src/Components/Footer/Footer.component.js
+++ src/Components/Footer/Footer.component.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ladies from '../../Constants/lady.constants';
-import './Footer.styles.css';
+import React from "react";
+import ladies from "../../Constants/lady.constants";
+import "./Footer.styles.css";
 
-const Footer = ({ ladiesIndex }) => (
-  <div className="ladys-name">
-    This Woman is {ladies[ladiesIndex].name}
-    <br></br>
-    {ladies[ladiesIndex].bioBlurb}
-  </div>
-);
+const Footer = ({ ladiesIndex }) => <div className="ladys-name">This Woman is {ladies[ladiesIndex].name}</div>;
 export default Footer;
