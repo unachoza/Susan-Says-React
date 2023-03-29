a4d2179abb1fe82b75b2318a82d5dc7bb00abade
arianna <unachoza@gmail.com>
2023-03-29 15:53:21 -0400

    fixed footer display removed trivia button positions

--- src/Components/Footer/Footer.component.js
+++ src/Components/Footer/Footer.component.js
@@ -2,5 +2,5 @@ import React from "react";
 import ladies from "../../Constants/lady.constants";
 import "./Footer.styles.css";
 
-const Footer = ({ ladiesIndex }) => <div className="ladys-name">This Woman is {ladies[ladiesIndex].name}</div>;
+const Footer = ({ ladiesIndex }) => <div className="ladys-name">{ladies[ladiesIndex].name}</div>;
 export default Footer;
--- src/Pages/ScorePage/ScorePage.component.js
+++ src/Pages/ScorePage/ScorePage.component.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import Trivia from '../../Components/Trivia/Trivia.component';
-import '../../Components/Buttons/button.styles.css';
-import './ScorePage.styles.css';
+import React from "react";
+import Trivia from "../../Components/Trivia/Trivia.component";
+import "../../Components/Buttons/button.styles.css";
+import "./ScorePage.styles.css";
 
 const ScorePage = ({ ladiesIndex }) => {
-  return (
-    <div className="page">
-      <Trivia ladiesIndex={ladiesIndex}/>
-    </div>
-  );
+  return <div className="page">{/* <Trivia ladiesIndex={ladiesIndex}/> */}</div>;
 };
 
 export default ScorePage;
